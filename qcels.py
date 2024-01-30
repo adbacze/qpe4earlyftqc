787854c0f9d03e76cd2fb525ee09a8d7774d5e74
jnelson621 <93797699+jnelson621@users.noreply.github.com>
2024-01-30 15:00:32 -0700

    Update qcels.py

--- qcels.py
+++ qcels.py
@@ -5,14 +5,8 @@ from cmath import exp
 def qcels(N, Ns, W, Usp, numQubits, repetitions,target):
 
     z = np.zeros(N,dtype=complex)
-    wCalls = 0
 
-    #stateprep = QuantumCircuit(numQubits)
-    #Usp(stateprep)
-    #stateprep = compileCT(stateprep,1e-4)
-    #Tsp = stateprep.count_ops().get('t')
     for l in range(N):
-        print(l)
         qcI = QuantumCircuit(numQubits)
         qcS = QuantumCircuit(numQubits)
         measurement = QuantumCircuit(numQubits,1)
@@ -26,22 +20,16 @@ def qcels(N, Ns, W, Usp, numQubits, repetitions,target):
             for k in range(repetitions):
                 W(qcI,target)
                 W(qcS,target)
-                wCalls += 2
+
         qcS.sdg(0)
         qcI.h(0)
         qcS.h(0)
-        #print(qcI.draw())
-        #qcI = compileCT(qcI,0.9*target)
-        #qcS = compileCT(qcS,0.9*target)
-        #numD1 = qcI.count_ops().get('t')
-        #numD2 = qcS.count_ops().get('t')
 
         measurement.barrier(range(numQubits))
         measurement.measure(0,0)
 
         qcI = qcI.compose(measurement)
         qcS = qcS.compose(measurement)
-        #print(qcI.draw())
 
         aer_sim = Aer.get_backend('aer_simulator')
         qcI_compiled = transpile(qcI, aer_sim)
@@ -68,7 +56,7 @@ def qcels(N, Ns, W, Usp, numQubits, repetitions,target):
             y = -1
 
         z[l] = complex(x,y)
-    return [z, wCalls]
+    return z
 
 def F(x, z, N, t):
     Z_fit=np.zeros(N,dtype = 'complex_')
@@ -78,23 +66,17 @@ def F(x, z, N, t):
 def maxF(z, N, t, est, bnds):
     fun = lambda x: F(x, z, N, t)
     result = minimize(fun,x0=[0.5,0,est],method = 'SLSQP',bounds=bnds)
-    #result = differential_evolution(fun, bounds = [(-1,1),(-1,1),(0,2*pi)])
     return result.x
 
 
-def qcelsT(N, Ns, W, Usp, numQubits, repetitions):
+def noisy_qcels(N, Ns, W, Usp, numQubits, repetitions, nm, precision, target):
 
     z = np.zeros(N,dtype=complex)
-    Tmax = 0
-    T = 0
 
     for l in range(N):
-
-        #print(l)
         qcI = QuantumCircuit(numQubits)
         qcS = QuantumCircuit(numQubits)
         measurement = QuantumCircuit(numQubits,1)
-
         qcI.h(0)
         qcS.h(0)
 
@@ -103,15 +85,18 @@ def qcelsT(N, Ns, W, Usp, numQubits, repetitions):
 
         for i in range(l):
             for k in range(repetitions):
-                W(qcI)
-                W(qcS)
+                W(qcI,target)
+                W(qcS,target)
 
         qcS.sdg(0)
         qcI.h(0)
         qcS.h(0)
 
-        qcI = compileCT(qcI,1e-4)
-        qcS = compileCT(qcS,1e-4)
+        qcI = compileCT2(qcI,precision)
+        qcS = compileCT2(qcS,precision)
+        numD1 = qcI.count_ops().get('t')
+        numD2 = qcS.count_ops().get('t')
+        T = numD1 + numD2
 
         measurement.barrier(range(numQubits))
         measurement.measure(0,0)
@@ -119,53 +104,32 @@ def qcelsT(N, Ns, W, Usp, numQubits, repetitions):
         qcI = qcI.compose(measurement)
         qcS = qcS.compose(measurement)
 
-        #print(qcI.draw())
+        aer_sim = AerSimulator(noise_model = nm)
+        qcI_compiled = transpile(qcI, aer_sim)
+        runs = Ns;
+        job_sim = aer_sim.run(qcI_compiled, shots=runs)
+        result_sim = job_sim.result()
+        counts = result_sim.get_counts(qcI_compiled)
 
-        numD1 = qcI.count_ops().get('t')
-        numD2 = qcS.count_ops().get('t')
+        if '0' in counts.keys():
+            x = 2*counts['0']/Ns -1
+        else:
+            x = -1
 
-        T = numD1 + numD2;
-        Tmax = max(Tmax,numD1,numD2);
+        aer_sim = AerSimulator(noise_model = nm)
+        qcS_compiled = transpile(qcS, aer_sim)
+        runs = Ns;
+        job_sim = aer_sim.run(qcS_compiled, shots=runs)
+        result_sim = job_sim.result()
+        counts = result_sim.get_counts(qcS_compiled)
 
-    return [T*Ns, Tmax]
+        if '0' in counts.keys():
+            y = 2*counts['0']/Ns -1
+        else:
+            y = -1
 
-
-"""
-N = 10
-rep = 1
-t = [10*2^(-5),10*2^-4,10*2^-3,10*2^-2,10*2^-1,10];
-Ns = 600
-
-for i in range(6):
-
-    z = qcels(N,Ns,W,Usp,3,rep);
-    if (i==0):
-        est = (-1*atan2(np.imag(z[1]),np.real(z[1])))%(2*pi)
-        bnds = [(-1,1),(-1,1),(0,2*pi)]
-
-    zfit = maxF(z,N,t,est,bnds)
-    rep = rep*2
-    est = zfit[2]
-    bnds = [(-1,1),(-1,1),(est-pi/rep,est+pi/rep)]
-    print(est)
-    print([theta ])
-
-
-
-
-print(theta)
-"""
-
-
-
-#print(t)
-
-
-
-
-
-
-
+        z[l] = complex(x,y)
+    return [z, T]
 
 
 #
