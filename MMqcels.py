d3dbbacef34d9713e63d4a320080fb665ea066c7
jnelson621 <93797699+jnelson621@users.noreply.github.com>
2024-01-31 09:18:01 -0700

    Update MMqcels.py

--- MMqcels.py
+++ MMqcels.py
@@ -1,5 +1,5 @@
 from Imports import *
-from scipy.optimize import minimize, shgo, least_squares, differential_evolution
+from scipy.optimize import minimize
 from cmath import exp
 
 def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
@@ -7,8 +7,6 @@ def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
     z = np.zeros(N,dtype=complex)
     t = np.zeros(N)
     k = pow(2,gen-1)
-    wCalls = 0
-    wCallsMax = 0
 
     for i in range(N):
         wCN = 0
@@ -33,21 +31,18 @@ def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
             for j in range(int(num)):
                 W(qcI,T)
                 W(qcS,T)
-                wCN += 2
             W(qcI,rem)
             W(qcS,rem)
-            wCN += 2
         elif(t[i] < 0):
             for j in range(int(num)):
                 W(qcI,-1*T)
                 W(qcS,-1*T)
-                wCN += 2
+
             W(qcI,-1*rem)
             W(qcS,-1*rem)
             wCN += 2
         t[i] = t[i]*k
-        wCallsMax = max(wCallsMax,wCN/2)
-        wCalls += wCN
+
 
         qcS.sdg(0)
         qcI.h(0)
@@ -85,7 +80,7 @@ def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
 
         z[i] = complex(x,y)
 
-    return [t,z,wCalls,wCallsMax]
+    return [t,z]
 
 def F(x, z, N, t):
     Z_fit=np.zeros(N,dtype = 'complex_')
@@ -100,18 +95,113 @@ def maxF(z, N, t, est, bnds):
 def MMQcels(N, Ns, sigmaT, T, W, Usp, numQubits, gens):
     bnds = [(-1,1),(-1,1),(-pi,pi)]
     est = 0
-    wCalls = 0
-    wCallsMax = 0
     for i in range(gens):
         z = dataGenerator(N,Ns,sigmaT,T,W,Usp,numQubits,i+1)
         zfit = maxF(z[1],N,z[0],est,bnds)
         est = zfit[2]
-        bnds = [(-1,1),(-1,1),(est-pi/(pow(2,i)*sigmaT),est+pi/(pow(2,i)*sigmaT))]
-        wCalls += z[2]
-        wCallsMax = max(wCallsMax,z[3])
-    print(wCalls)
-    return [est,wCalls,wCallsMax]
+        bnds = [(-1,1),(-1,1),(est-pi/(pow(2,i)*sigmaT),est+pi/(pow(2,i)*sigmaT))
+    return est
 
+
+def noisyDataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen, nm, precision):
+
+    z = np.zeros(N,dtype=complex)
+    t = np.zeros(N)
+    k = pow(2,gen-1)
+
+    for i in range(N):
+        wCN = 0
+        qcI = QuantumCircuit(numQubits)
+        qcS = QuantumCircuit(numQubits)
+        measurement = QuantumCircuit(numQubits,1)
+
+        qcI.h(0)
+        qcS.h(0)
+        #stateprep
+        Usp(qcI)
+        Usp(qcS)
+        #Random Time Evoloution
+        while(True): #instead of sampling from a truncated Gaussian, I will throw out a t value greater than a set maximum
+            t[i] = np.random.normal(0,sigmaT,1)
+            if(abs(t[i]) < T):
+                break
+
+        num = abs(k*t[i])//T
+        rem = abs(k*t[i])%T
+        if(t[i] >= 0):
+            for j in range(int(num)):
+                W(qcI,T)
+                W(qcS,T)
+                wCN += 2
+            W(qcI,rem)
+            W(qcS,rem)
+            wCN += 2
+        elif(t[i] < 0):
+            for j in range(int(num)):
+                W(qcI,-1*T)
+                W(qcS,-1*T)
+            W(qcI,-1*rem)
+            W(qcS,-1*rem)
+        t[i] = t[i]*k
+
+        qcS.sdg(0)
+        qcI.h(0)
+        qcS.h(0)
+
+        qcI = compileCT2(qcI,precision)
+        qcS = compileCT2(qcS,precision)
+
+        T = qcI.count_ops().get('t')+qcS.count_ops.get('t')
+
+        measurement.barrier(range(numQubits))
+        measurement.measure(0,0)
+
+        qcI = qcI.compose(measurement)
+        qcS = qcS.compose(measurement)
+
+        aer_sim = AerSimulator(noise_model = nm)
+        qcI_compiled = transpile(qcI, aer_sim)
+        runs = Ns;
+        job_sim = aer_sim.run(qcI_compiled, shots=runs)
+        result_sim = job_sim.result()
+        counts = result_sim.get_counts(qcI_compiled)
+
+        if '0' in counts.keys():
+            x = 2*counts['0']/Ns -1
+        else:
+            x = -1
+
+        aer_sim = AerSimulator(noise_model = nm)
+        qcS_compiled = transpile(qcS, aer_sim)
+        runs = Ns;
+        job_sim = aer_sim.run(qcS_compiled, shots=runs)
+        result_sim = job_sim.result()
+        counts = result_sim.get_counts(qcS_compiled)
+
+        if '0' in counts.keys():
+            y = 2*counts['0']/Ns -1
+        else:
+            y = -1
+
+        z[i] = complex(x,y)
+
+    return [t,z,T]
+
+def NoisyMMQcels(N, Ns, sigmaT, T, W, Usp, numQubits, gens, nm, precision):
+    bnds = [(-1,1),(-1,1),(-pi,pi)]
+    est = 0
+    Ttot = 0
+    Tmax = 0
+    for i in range(gens):
+        z = noisyDataGenerator(N,Ns,sigmaT,T,W,Usp,numQubits,i+1,nm,precision)
+        zfit = maxF(z[1],N,z[0],est,bnds)
+        est = zfit[2]
+        bnds = [(-1,1),(-1,1),(est-pi/(pow(2,i)*sigmaT),est+pi/(pow(2,i)*sigmaT))]
+        Ttot += Ns*z[2]
+        Tmax = max(Tmax,z[2])
+    return [est,Ttot,Tmax]
+
+#
 
 
 
