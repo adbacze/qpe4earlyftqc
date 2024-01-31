2a735cbe928b0262b09baa6310f9b2fafa5a8595
jnelson621 <93797699+jnelson621@users.noreply.github.com>
2024-01-31 10:39:34 -0700

    Update MMqcels.py

--- MMqcels.py
+++ MMqcels.py
@@ -2,6 +2,19 @@ from Imports import *
 from scipy.optimize import minimize
 from cmath import exp
 
+#This script provides methods to simulate multi-modal quantum complex exponetenial least squares (MMQCELS) as described in https://arxiv.org/pdf/2303.05714.pdf 
+#Two versions of this algorithm are provided; "MMQcels" performs noiseless simulations without compiling circuits into Clifford+Ts while "noisy_MMQcels" allows for the addition
+#of a noise model as well as compiles all circuits into Clifford+Ts while tracking the Tmax and Ttot.
+#Since MMQcels does not compile circuits into Clifford+Ts it runs much faster.
+#Because Ns is typically much lower, MMQcels takes longer to simulate using qiskit than qcels or arpe. 
+
+#Both MMQcels and noisy_MMQcels utilize a dataGenerator function. 
+#This function generates N data pairs 
+#MMQcels and noisy_MMQcels calls the dataGenerator function then performs a least squares fit on the generated data
+#Additionally noisy_MMQcels also returns the Ttot and Tmax.
+
+#N = number of data pairs, Ns = number of samples to produce each data pair, sigmaT = width of normal distribution from which Hamiltonian simulation time is selected,
+#T = maximum Hamiltonian simulation time, W = function to implement time evolution operator, Usp = function to implement state prep, numQubits = number of qubits including ancilla, gen = current generation 
 def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
 
     z = np.zeros(N,dtype=complex)
@@ -80,18 +93,20 @@ def dataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen):
 
         z[i] = complex(x,y)
 
-    return [t,z]
+    return [t,z] #return times and z
 
-def F(x, z, N, t):
+def F(x, z, N, t): #Define function to minimized for least squares fit
     Z_fit=np.zeros(N,dtype = 'complex_')
     Z_fit=(x[0]+1j*x[1])*np.exp(-1j*x[2]*t)
     return (np.linalg.norm(Z_fit-z)**2/N)
 
-def maxF(z, N, t, est, bnds):
+def maxF(z, N, t, est, bnds): #z = time series data generated form qcels, N = number of data pairs, t = corresponding times for time series, est = prior estimate, bnds = bounds for fit
     fun = lambda x: F(x, z, N, t)
     result = minimize(fun,x0=[0.5,0,est],method = 'SLSQP',bounds=bnds)
     return result.x
 
+#N = number of data pairs per generation, number of samples per data pair, sigmaT = initial width of normal distrubtion of Hamiltonian simulation time (doubles every subsequent generation)
+#T = initial maximum Hamiltonian simulation time (doubles every subsequent generation, W = function to implement time evolution operator, Usp = function to implement state prep, numQubits = number of qubits including ancilla, gens = number of generations to run
 def MMQcels(N, Ns, sigmaT, T, W, Usp, numQubits, gens):
     bnds = [(-1,1),(-1,1),(-pi,pi)]
     est = 0
@@ -102,7 +117,9 @@ def MMQcels(N, Ns, sigmaT, T, W, Usp, numQubits, gens):
         bnds = [(-1,1),(-1,1),(est-pi/(pow(2,i)*sigmaT),est+pi/(pow(2,i)*sigmaT))
     return est
 
-
+#N = number of data pairs, Ns = number of samples to produce each data pair, sigmaT = width of normal distribution from which Hamiltonian simulation time is selected,
+#T = maximum Hamiltonian simulation time, W = function to implement time evolution operator, Usp = function to implement state prep, numQubits = number of qubits including ancilla, gen = current generation 
+#nm = noise model, precision = rotation synthesis precision for each Z-rotation
 def noisyDataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen, nm, precision):
 
     z = np.zeros(N,dtype=complex)
@@ -151,7 +168,7 @@ def noisyDataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen, nm, precision):
         qcI = compileCT2(qcI,precision)
         qcS = compileCT2(qcS,precision)
 
-        T = qcI.count_ops().get('t')+qcS.count_ops.get('t')
+        T_count = qcI.count_ops().get('t')+qcS.count_ops.get('t')
 
         measurement.barrier(range(numQubits))
         measurement.measure(0,0)
@@ -185,8 +202,11 @@ def noisyDataGenerator(N, Ns, sigmaT, T, W, Usp, numQubits, gen, nm, precision):
 
         z[i] = complex(x,y)
 
-    return [t,z,T]
+    return [t,z,T_count]
 
+#N = number of data pairs per generation, number of samples per data pair, sigmaT = initial width of normal distrubtion of Hamiltonian simulation time (doubles every subsequent generation)
+#T = initial maximum Hamiltonian simulation time (doubles every subsequent generation, W = function to implement time evolution operator, Usp = function to implement state prep, numQubits = number of qubits including ancilla, gens = number of generations to run
+#nm = noise model, precision = rotation sysnthesis precision for each Z-rotation
 def NoisyMMQcels(N, Ns, sigmaT, T, W, Usp, numQubits, gens, nm, precision):
     bnds = [(-1,1),(-1,1),(-pi,pi)]
     est = 0
